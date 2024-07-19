6b4ec07f8589392137ec1d7026f60babdb0e300a
sctlcd <lucinda.souchet@gmail.com>
2024-07-19 15:22:57 +0200

    1st pass on the component shape

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className="App">
       <div className='container mt-3 text-center'>
-        <div classNAme='card bg-light m-auto'>
-          <h1>Length</h1>
+        <div classNAme='card bg-light m-auto style={{width: 200}}'>
+          <h1
+            className='text-light card border-50'
+            style={{height:150, width:150, border: '2px solid #666'}}
+          >Length</h1>
         </div>
       </div>
     </div>
