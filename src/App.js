7383823e15db5cd9f754e7d9b5f82ddc7262847e
sctlcd <lucinda.souchet@gmail.com>
2024-09-18 18:52:49 +0200

    added length value using useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.scss';
 import Button from "./components/Button";
+import React, {useState} from "react";
 
 function App() {
+  const [length, setlength] = useState(0);
+
   return (
     <div className="App">
       <div className='container mt-3 mb-3 text-center d-flex'>
@@ -10,7 +13,7 @@ function App() {
             className='text-light card border-50'
             style={{height:150, width:150, border: '2px solid #666'}}
           >
-            Length
+            {length}
           </h1>
           <div className='d-flex my-2'>
             <Button text="-" btnClass={"btn-lg border-10"} onClick={() => console.log("-")} />
