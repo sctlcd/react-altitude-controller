51a416a88b980cc9b4d860eefe2c1e62fade83a1
sctlcd <lucinda.souchet@gmail.com>
2024-09-23 18:32:49 +0200

    app container centered verticaly

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className='container mt-3 mb-3 text-center d-flex'>
+      <div className='container mt-3 mb-3 text-center d-flex vertical-center'>
         <div className='card bg-light m-auto style={{width: 200}} px-2 pt-2'>
           <h1
-            className={`text-light card border-50 ${altitude > 0 ? 'bg-danger' : 'bg-info'}`}
+            className={`text-light fs-lg card border-50 ${altitude > 0 ? 'bg-danger' : 'bg-info'}`}
             style={{height:150, width:150, border: '2px solid #666'}}
           >
             {altitude} m
